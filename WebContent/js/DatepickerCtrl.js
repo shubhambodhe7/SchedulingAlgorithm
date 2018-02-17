e0cb5458221c8ddc5ab5ede516cb84f6a180ccb4
ReshmaNeelam <35306096+ReshmaNeelam@users.noreply.github.com>
2018-02-17 20:08:25 +0530

    delete and view holidays

--- WebContent/js/DatepickerCtrl.js
+++ WebContent/js/DatepickerCtrl.js
@@ -1,35 +1,111 @@
-app.controller('DatepickerCtrl', function ($scope) {
-  $scope.today = function() {
-    $scope.dt = new Date();
-  };
-  $scope.today();
+app
+		.controller(
+				'DatepickerCtrl',
+				function($scope, $http) {
+					
+					
+					
+			$scope.holiday = { dt : new Date(),
+					occasion : "" };
+	
+					$scope.clear = function() {
+						$scope.dt = null;
+					};
 
-  $scope.clear = function () {
-    $scope.dt = null;
-  };
+					$scope.deleteHoliday = function(rowId) {
+						$http
+						.get('project/deletePublicHoliday/' + rowId)
+						.then(
+								function(response) {
+									console.log(response.data);
+									if (response.data > 0) {
+										bootbox
+												.alert("Public holiday deleted successful");
+												getAllPublicHoliday();
+									} else {
+										bootbox
+												.alert("Public holiday deletion failed");
+									}
+									
+									
+								
+								},
+								function error(response) {
+									console.log(response);
 
-  // Disable weekend selection
-  $scope.disabled = function(date, mode) {
-    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-  };
+									bootbox
+											.alert("Error in deleting holiday!");
 
-  $scope.toggleMin = function() {
-    $scope.minDate = $scope.minDate ? null : new Date();
-  };
-  $scope.toggleMin();
+								});
+					};
 
-  $scope.open = function($event) {
-   /* $event.preventDefault();
-    $event.stopPropagation();*/
+					$scope.addPublicHoliday = function(holiday) {
+						console.log(holiday);
 
-    $scope.opened = true;
-  };
+						if (holiday.dt == null){
+							bootbox
+							.alert("Please select date!");
+						} else if (holiday.occasion === ""){
+							bootbox
+							.alert("Please enter occasion name!");
+						} else {
+						
+						
+						$http
+								.get(
+										'project/addPublicHoliday/'
+												+ holiday.dt + '/'
+												+ holiday.occasion)
+								.then(
+										function(response) {
+											console.log(response.data);
+											if (response.data > 0) {
+												bootbox
+														.alert("Public holiday added successfully");
+														getAllPublicHoliday();
+											} else {
+												bootbox
+														.alert("Public holiday addition failed");
+											}
 
-  $scope.dateOptions = {
-    formatYear: 'yy',
-    startingDay: 1
-  };
+										},
+										function error(response) {
+											console.log(response);
 
-  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-  $scope.format = $scope.formats[0];
-});
+											bootbox
+													.alert("Public holiday addition failed");
+
+										});
+						};
+					};
+					getAllPublicHoliday();
+					function getAllPublicHoliday() {
+
+						$http
+								.get('project/getPublicHoliday')
+								.then(
+										function(response) {
+											console.log(response.data);
+											$scope.publicHolidays = response.data;
+											
+											for (var i = 0; i < $scope.publicHolidays.length; ++i) {
+												
+												viewDate = new Date($scope.publicHolidays[i].date);
+												$scope.publicHolidays[i].date = viewDate.toDateString();
+											}
+											
+											
+										
+										},
+										function error(response) {
+											console.log(response);
+
+											bootbox
+													.alert("Error in fetching holidays list!");
+
+										});
+					}
+					;
+
+			
+				});
